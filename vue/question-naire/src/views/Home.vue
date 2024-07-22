8317518aff55856d30fa2162a74767e7486ae456
云卿 <1725541407@qq.com>
2024-07-22 21:36:00 +0800

    9

---
+++ vue/question-naire/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <ItemContainer parent="home" />
+    
+</template>
+
+<script setup>
+import ItemContainer from '@/components/ItemContainer.vue';
+</script>
+
+<style lang="css" scoped>
+
+</style>
